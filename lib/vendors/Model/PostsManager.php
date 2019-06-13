781e35f756b1025c4d6819426f1592e528f7c396
radalaaeddine <rad.alaaeddine@gmail.com>
2019-06-13 10:49:15 +0200

    Class parente de la class PostsManagerPDO(), implimente la methode getList

---
+++ lib/vendors/Model/PostsManager.php
@@ -0,0 +1,15 @@
+<?php
+namespace Model;
+
+use \OCFram\Manager;
+
+abstract class PostsManager extends Manager
+{
+  /**
+   * Méthode retournant une liste de news demandée
+   * @param $debut int La première news à sélectionner
+   * @param $limite int Le nombre de news à sélectionner
+   * @return array La liste des news. Chaque entrée est une instance de News.
+   */
+  abstract public function getList($debut = -1, $limite = -1);
+}
