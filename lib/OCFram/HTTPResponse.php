3925b92035d3dab40df588d0fcff8ee25f7f80c6
radalaaeddine <rad.alaaeddine@gmail.com>
2019-06-13 08:25:30 +0200

    Redirection vers la page d'erreur 404

--- lib/OCFram/HTTPResponse.php
+++ lib/OCFram/HTTPResponse.php
@@ -18,7 +18,14 @@ class HTTPResponse
 
   public function redirect404()
   {
-    
+    //créer une instance de la classe Page que l'on stocke dans l'attribut page
+    $this->page = new Page($this->app);
+    //On assigne ensuite à la page le fichier qui fait office de vue à générer
+    $this->page->setContentFile(__DIR__.'/../../Errors/404.html');
+    //On ajoute un header disant que le document est non trouvé
+    $this->addHeader('HTTP/1.0 404 Not Found');
+    //On envoie la réponse
+    $this->send();
   }
   
   public function send()
