78484071522e14a59a821e1039970407ffc0d8ff
radalaaeddine <rad.alaaeddine@gmail.com>
2019-06-13 08:22:00 +0200

    Instruction pour informer la page quelle vue a afficher

--- lib/OCFram/BackController.php
+++ lib/OCFram/BackController.php
@@ -62,7 +62,9 @@ abstract class BackController extends ApplicationComponent
     {
       throw new \InvalidArgumentException('La vue doit être une chaine de caractères valide');
     }
-
+    
     $this->view = $view;
+    
+    $this->page->setContentFile(__DIR__.'/../../App/'.$this->app->name().'/Modules/'.$this->module.'/Views/'.$this->view.'.php');
   }
 }
