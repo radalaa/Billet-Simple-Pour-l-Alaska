cdd2f59d123abb0675ef5f4a077a94f9c4fb6b2e
radalaaeddine <rad.alaaeddine@gmail.com>
2019-06-19 20:54:25 +0200

    view pour modifier les posts

---
+++ App/Backend/Modules/Posts/Views/update.php
@@ -0,0 +1,33 @@
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alaa
+ * Date: 02/06/2019
+ * Time: 08:29
+ */
+
+		?>
+          <li><a href="/">Voir le site</a></li>
+          <?php if ($user->isAuthenticated()) { ?>
+           <li><a href="/admin/gestion">Gestion de blog</a></li>
+          <li><a href="/admin/logout">Déconnexion</a></li>
+          <?php } ?>
+        </ul>
+      </div>
+      <!--/.nav-collapse -->
+    </div>
+  </div>
+<div class="amin-section container">
+
+<h2>Modifier</h2>
+<form action="" method="post">
+  <p>
+    <?= $form ?>
+    
+    <input type="submit" value="Modifier" />
+  </p>
+</form>
+
+</div>
