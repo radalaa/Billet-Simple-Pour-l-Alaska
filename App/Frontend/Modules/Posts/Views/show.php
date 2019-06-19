5c8e3eb995a48d745d960054d68c573381ed0e18
radalaaeddine <rad.alaaeddine@gmail.com>
2019-06-19 21:16:20 +0200

    view pour affciher une page selectionnée

---
+++ App/Frontend/Modules/Posts/Views/show.php
@@ -0,0 +1,72 @@
+
+
+
+		<?php 
+		foreach ($listeMenu as $menu)
+		{
+		?>
+		    <li><a href="menu-<?= $menu['id'] ?>.html"><?= $menu['menu'] ?></a></li>     
+		<?php
+		}
+		?>
+
+ 		  <li class="active"><a href="posts.html" class="smoothscroll">Les romans</a></li>
+          <?php if ($user->isAuthenticated()) { ?>
+          <li><a href="/admin/gestion">Admin</a></li>
+          <?php } ?>
+        </ul>
+      </div>
+      <!--/.nav-collapse -->
+    </div>
+  </div>
+
+ <!----------------------------------------------------------------------------------->
+ <section id="works"></section>
+  <div class="container">
+    <div class="row centered mt mb">
+      	<div class="col-lg-8 col-lg-offset-2">
+
+
+
+<p>Par <em><?= $posts['name'] ?></em>, le <?= $posts['created']->format('d/m/Y à H\hi') ?></p>
+<h2><?= $posts['slug'] ?></h2>
+
+<p><?= nl2br($posts['content']) ?></p>
+
+<?php if ($posts['created'] != $posts['modified']) { ?>
+  <p style="text-align: right;"><small><em>Modifiée le <?= $posts['modified']->format('d/m/Y à H\hi') ?></em></small></p>
+<?php } ?>
+
+<p><a id="add-comment" href="commenter-<?= $posts['id'] ?>.html">Ajouter un commentaire</a></p>
+
+<?php
+if (empty($comments))
+{
+?>
+<p>Aucun commentaire n'a encore été posté. Soyez le premier à en laisser un !</p>
+<?php
+}
+
+foreach ($comments as $comment)
+{
+?>
+<fieldset>
+  <legend>
+    Posté par <strong><?= htmlspecialchars($comment['auteur']) ?></strong> le <?= $comment['date']->format('d/m/Y à H\hi') ?>
+    <?php if ($user->isAuthenticated()) { ?> -
+      <a href="admin/comment-update-<?= $comment['id'] ?>.html">Modifier</a> |
+      <a href="admin/comment-delete-<?= $comment['id'] ?>.html">Supprimer</a>
+    <?php } ?>
+  </legend>
+  <p><?= nl2br($comment['contenu']) ?></p>
+</fieldset>
+<?php
+}
+?>
+
+<p><a class="ajouter" href="commenter-<?= $posts['id'] ?>.html">Ajouter un commentaire</a></p>
+
+
+</div>
+    </div>
+  </div>
